082ea22da6250896118863e77e3f7b74a4e2fefc
Vitali Stsepaniuk <vstepanuyk@gmail.com>
2020-03-31 12:03:37 +0200

    refactor: login page (ui/ux)

--- pages/login.js
+++ pages/login.js
@@ -31,7 +31,7 @@ export default function Login() {
 
   return (
     <>
-      <Backdrop open={loading}>
+      <Backdrop open={loading} style={{ zIndex: 1 }}>
         <CircularProgress color="inherit"/>
       </Backdrop>
       <Snackbar
