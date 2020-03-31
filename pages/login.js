fa54974433565315547c58ea4b7cc1827233a28f
Vitali Stsepaniuk <vstepanuyk@gmail.com>
2020-03-31 11:55:34 +0200

    refactor: login page (ui/ux)

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,10 @@
 import Backdrop from '@material-ui/core/Backdrop'
+import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
-import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
+import Snackbar from '@material-ui/core/Snackbar'
+import Alert from '@material-ui/lab/Alert'
 import firebase from 'firebase'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
@@ -11,17 +13,19 @@ import Copyright from '../src/Copyright'
 export default function Login() {
   const router = useRouter()
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const signIn = async () => {
-    setLoading(true);
+    setLoading(true)
+    error && setError(null)
 
     try {
-      await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
-      await router.push('/');
+      await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      await router.push('/')
     } catch (err) {
-      console.error(err);
+      setError(err.message)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
   }
 
@@ -30,17 +34,28 @@ export default function Login() {
       <Backdrop open={loading}>
         <CircularProgress color="inherit"/>
       </Backdrop>
-      <Container maxWidth="sm">
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-        >
-          <Button onClick={signIn}>Login with Google</Button>
+      <Snackbar
+        anchorOrigin={{ horizontal: "center", vertical: "top" }}
+        open={error !== null}
+        autoHideDuration={10000}
+        onClose={() => setError(null)}>
+        <Alert onClose={() => setError(null)} severity="error">{error}</Alert>
+      </Snackbar>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={6}>
+          <Box m={1}>
+            <Button onClick={signIn} color="primary" variant="contained">Login with Google</Button>
+          </Box>
+          <Copyright/>
         </Grid>
-        <Copyright/>
-      </Container>
+      </Grid>
     </>
   )
 }
