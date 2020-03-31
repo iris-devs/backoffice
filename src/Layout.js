461565343a309e33b57ded7bd5e7fa28e2a6a480
Vitali Stsepaniuk <vstepanuyk@gmail.com>
2020-03-31 18:26:05 +0200

    refactor: clean styles

--- src/Layout.js
+++ src/Layout.js
@@ -181,14 +181,5 @@ const useStyles = makeStyles(theme => ({
   container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 240,
-  },
+  }
 }))
